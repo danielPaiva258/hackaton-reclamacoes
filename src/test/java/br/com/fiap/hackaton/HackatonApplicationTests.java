e0db54156c9f0116ce7218d6d2a1220a928796ee
Willian Andrade <williandrade@gmail.com>
2023-05-03 21:09:20 -0300

    +Autenticação

--- src/test/java/br/com/fiap/hackaton/HackatonApplicationTests.java
+++ src/test/java/br/com/fiap/hackaton/HackatonApplicationTests.java
@@ -8,7 +8,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-@SpringBootTest
+@SpringBootTest()
 class HackatonApplicationTests {
 
 	@Autowired
@@ -17,22 +17,32 @@ class HackatonApplicationTests {
 
 	@Test
 	void contextLoads() {
+		log("Testes de contexto OK!");
 	}
 
 	@Test
 	void recupera_reclamacao_por_id() {
+		log("Iniciando teste: Recupera Reclamação por ID");
 		Integer id = 1;
+		log("1- Recuperando a reclamação...");
 		Reclamacao reclamacao = reclamacaoRepository.findById(id).get();
+		log("2 - Preparando dados para comparacação");
 		String origem = "Telefone";
 		String status = "Aberta";
 		String titulo = "Problema com a linha";
 		Integer idCliente = 1;
+
+		log("3 - Comparações");
 
 		assertEquals(origem, reclamacao.getOrigem());
 		assertEquals(status, reclamacao.getStatus());
 		assertEquals(titulo, reclamacao.getTitulo());
 		assertEquals(idCliente, reclamacao.getIdCliente());
 
+	}
+
+	private void log(String msg){
+		//System.out.println(String.format("LOG: %s", msg));
 	}
 
 
